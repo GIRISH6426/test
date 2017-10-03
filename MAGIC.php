26679eba98c7e3fdc489c56938bb3eee36237f45
Girish <girish6426@gmail.com>
2017-10-03 16:20:08 +0530

    add files

---
+++ MAGIC.php
@@ -0,0 +1,29 @@
+<DOCTYPE>
+    
+
+
+<html>
+<head>
+<title>REGISTRATION FORM</title>
+</head>
+<body>
+<form action="userlist.php" method="POST">
+<table border="1" width="500" height="400" align="center">
+<tr>
+<td colspan="5" align="center" bgcolor="#FF6699">INFORMATION</td>
+</tr>
+<tr>
+<td align="center" bgcolor="#999999">Enter your Number</td>
+<td align="center" bgcolor="#CCCCCC"><input type="text" name="username" value="" size="30"/></td></tr>
+
+
+<tr>
+<td colspan="5" align="center" bgcolor="#9999FF"><input type="Submit" name="SUBMIT" value="SUBMIT"/></td>
+
+</tr>
+
+
+</table>
+</form>
+</body>
+</html>
